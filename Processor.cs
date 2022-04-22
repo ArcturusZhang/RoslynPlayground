752cec5d59de9cc3e1c2b94a2491853e030008a5
ArcturusZhang <dapzhang@microsoft.com>
2022-04-22 14:00:46 +0800

    some play around SymbolFinder

---
+++ Processor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FindSymbols;
+
+namespace RoslynPlayground
+{
+    internal static class Processor
+    {
+        public static async Task<Project> Process(Project project)
+        {
+            await foreach (var c in GetPublicClasses(project))
+            {
+                if (c.Identifier.Text.Equals("A"))
+                {
+                    await FindReferences(project, c);
+                }
+            }
+            return project;
+        }
+
+        private static async Task FindReferences(Project project, ClassDeclarationSyntax classDeclaration)
+        {
+            var compilation = await project.GetCompilationAsync();
+            var sematicModel = compilation!.GetSemanticModel(classDeclaration.SyntaxTree);
+            var symbol = sematicModel.GetDeclaredSymbol(classDeclaration);
+            foreach (var reference in await SymbolFinder.FindReferencesAsync(symbol!, project.Solution))
+            {
+                foreach (var location in reference.Locations)
+                {
+                    Console.WriteLine($"SyntaxNode {classDeclaration.Identifier.Text} is referenced by document {location.Document.Name}");
+                }
+            }
+        }
+
+        private static async IAsyncEnumerable<ClassDeclarationSyntax> GetPublicClasses(Project project)
+        {
+            foreach (var document in project.Documents)
+            {
+                var root = await document.GetSyntaxRootAsync();
+                if (root == null)
+                    continue;
+                var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+                if (classes.Any())
+                    yield return classes.First();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.CodeAnalysis;
+using RoslynPlayground;
+
+const string rootClassContent = @"
+public class RootClass
+{
+    public void DoSomething()
+    {
+        var s = new A();
+        s.Try();
+    }
+
+    internal A A {get;set;}
+}
+";
+
+const string aClassContent = @"
+public class A
+{
+    public void Try()
+    {
+        // do nothing
+    }
+}
+";
+
+var workspace = new AdhocWorkspace();
+var project = workspace.AddProject("Playground", LanguageNames.CSharp);
+project = project.AddDocument("RootClass.cs", rootClassContent).Project;
+project = project.AddDocument("A.cs", aClassContent).Project;
+
+project = await Processor.Process(project);
